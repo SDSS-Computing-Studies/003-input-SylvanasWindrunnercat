4d094a25aec937c296af70bb955cfdd404d47157
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-11-13 14:13:40 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -11,3 +11,14 @@
 # volume
 #
 # test output radius of 3 should give volume of 84.8230016469
+radius=input("Enter the radius")
+
+import math
+
+P=math.pi
+
+r=int(radius)
+
+volume=4/3*P*r**3
+
+print(volume)
