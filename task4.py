88ca59110d8f926d6fa3852f8b606d5bf64fc956
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-11-13 14:14:14 -0800

    Update task4.py

--- task4.py
+++ task4.py
@@ -13,3 +13,19 @@
 #
 # Test output
 # input sides of 5 and 7 should give hypotenuse of 8.60232526704
+
+side1=input("The side1 is ")
+
+side2=input("The side2 is ")
+
+side1=int(side1)
+
+side2=int(side2)
+
+import math
+
+hypotenuse=side1**2+side2**2
+
+hypotenuse=math.sqrt(hypotenuse)
+
+print(hypotenuse)
