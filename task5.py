436852cb19d400c6576148cff747fa035e33fbef
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-11-13 14:14:29 -0800

    Update task5.py

--- task5.py
+++ task5.py
@@ -13,3 +13,19 @@
 # Note: You will need to do some strange things with your cube root.
 # Remember that a cube root is the same as an exponent of 1/3, but
 # here you will need to do a power of 1.0/3 or something strange happens.
+
+volume=input("The volume is ")
+
+volume=float(volume)
+
+import math
+
+P=math.pi
+
+radius=volume*3/4/P
+
+radius=math.pow(radius, 1.0/3)
+
+radius=float(radius)
+
+print(radius)
